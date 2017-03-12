8b978de0612b378dc223a1ec6c37ef769753659f
TaiKi Kim <ktk6176@naver.com>
2017-03-12 22:33:08 +0900

    create modal_login

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,28 @@
-$(document).ready(function(){
-  setTimeout(slide, 300)
+$(function(){
+  const $mainLog = $(".main_log");
+  const $modal = $(".login_form");
+  const $btn = $("#login_btn");
+  const $cancle_btn = $(".login_cancle");
+
+  $btn.click(function() {
+    $modal.css("display", "block");
+  });
+
+  $cancle_btn.click(function(){
+    $modal.css("display", "none");
+  });
+
+  $(document).click(function(event){
+    if(event.target == $modal){
+      $modal.css("display", "none");
+    }
+  });
+  slide();
+
+  function slide() {
+    $mainLog.animate({
+      right:'0px'
+    },800, "swing")
+  }
 
 });
-function slide() {
-  $(".main_log").animate({
-    right:'0px'
-  },800, "swing")
-}
