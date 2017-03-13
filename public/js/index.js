5580b4ac796578f1b9dbc36b00f5ff7d9cb3f891
TaiKi Kim <ktk6176@naver.com>
2017-03-13 22:53:34 +0900

    create making thumnail html form

--- public/js/index.js
+++ public/js/index.js
@@ -1,23 +1,43 @@
 $(function(){
   const $mainLog = $(".main_log");
-  const $modal = $(".login_form");
-  const $btn = $("#login_btn");
+  const $login = $(".login_form");
+  const $regist = $(".regist_form");
+  const $btn1 = $("#login_btn");
+  const $btn2 = $("#regist_btn");
   const $cancle_btn = $(".login_cancle");
+  const $cancle = $("#cancle");
 
-  $btn.click(function() {
-    $modal.css("display", "block");
-  });
 
-  $cancle_btn.click(function(){
-    $modal.css("display", "none");
-  });
 
-  $(document).click(function(event){
-    if(event.target == $modal){
-      $modal.css("display", "none");
-    }
-  });
+
   slide();
+
+  LoginConsole($login, $btn1);
+  LoginConsole($regist, $btn2);
+
+
+
+
+
+  function LoginConsole(thing, btn) {
+    btn.click(function() {
+      thing.css("display", "block");
+    });
+
+    $cancle_btn.click(function(){
+      thing.css("display", "none");
+    });
+
+    $(document).click(function(event){
+      if(event.target == thing){
+        thing.css("display", "none");
+      }
+    });
+
+    $cancle.click(function() {
+      $regist.css("display", "none");
+    })
+  }
 
   function slide() {
     $mainLog.animate({
