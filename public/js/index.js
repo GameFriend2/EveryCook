58e80941f1186403e3a588d8ba9d41577e15b06b
TaiKi Kim <ktk6176@naver.com>
2017-03-15 22:38:12 +0900

    sub_item_index Create
    sub_item_index Create

--- public/js/index.js
+++ public/js/index.js
@@ -6,9 +6,21 @@ $(function(){
   const $btn2 = $("#regist_btn");
   const $cancle_btn = $(".login_cancle");
   const $cancle = $("#cancle");
+  const $add_button = $(".item_add");
+  const $subItemPot = $(".sub_item_pot");
+  const $subItemDel = $(".sub_del");
+  let index = 4;
+  $add_button.click(function(){
+    var tag = "<div class='item_list'><input class='sub_item item"+index + "' type='text' name='' value='' placeholder='요리에 필요한 부재료를 적어주세요'></input><button type='button' name='button'>&times;</button></div>"
+    console.log(tag);
+    $subItemPot.append(tag);
+    index++;
+  });
 
-
-
+  $subItemDel.click(function(){
+    $subItemDel.parent.remove();
+    // $subItemDel.remove();
+  })
 
   slide();
 
