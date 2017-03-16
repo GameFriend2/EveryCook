6a224d763a7b8be6e7fd3f38ec76e186ad97ae2b
TaiKi Kim <ktk6176@naver.com>
2017-03-16 22:57:15 +0900

    Create "Remove button"
    sub_item and sos_item create remove function and add function

--- public/js/index.js
+++ public/js/index.js
@@ -7,20 +7,38 @@ $(function(){
   const $cancle_btn = $(".login_cancle");
   const $cancle = $("#cancle");
   const $add_button = $(".item_add");
+  const $add_button2 = $(".item_add2")
   const $subItemPot = $(".sub_item_pot");
-  const $subItemDel = $(".sub_del");
+  const $subSosPot = $(".sub_sos_pot")
+
   let index = 4;
+
+
+
   $add_button.click(function(){
-    var tag = "<div class='item_list'><input class='sub_item item"+index + "' type='text' name='' value='' placeholder='요리에 필요한 부재료를 적어주세요'></input><button type='button' name='button'>&times;</button></div>"
-    console.log(tag);
+    var tag = "<div class='item_list'><input class='sub_item' type='text' name='' value='' placeholder='요리에 필요한 부재료를 적어주세요'></input><button class='sub_del' type='button' name='button'>&times;</button></div>"
+
     $subItemPot.append(tag);
-    index++;
+
+    $(".sub_del").on('click', function() {
+      $(this).parent().remove();
+    });
   });
 
-  $subItemDel.click(function(){
-    $subItemDel.parent.remove();
-    // $subItemDel.remove();
+  $add_button2.click(function(){
+    var tag2 = "<div class='sos_list'><input class='sub_item' type='text' name='' value='' placeholder='요리에 필요한 소스를 적어주세요'></input><button class='sub_del' type='button' name='button'>&times;</button></div>"
+
+    $subSosPot.append(tag2);
+
+    $(".sub_del").on('click', function() {
+      $(this).parent().remove();
+    });
   })
+
+  // $subItemDel.click(function(){
+  //   $(this).parent.remove();
+  //   // $subItemDel.remove();
+  // })
 
   slide();
 
